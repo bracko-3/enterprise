7974b519802f9dee369c663cf6a0b5cf024be3c3
bracko-3 <cummijb@mail.uc.edu>
2023-09-07 14:42:37 -0400

    Working with Specimen DTO

--- src/main/java/com/myplantdiary/enterprise/PlantDiaryController.java
+++ src/main/java/com/myplantdiary/enterprise/PlantDiaryController.java
@@ -1,6 +1,7 @@
 package com.myplantdiary.enterprise;
 
 
+import com.myplantdiary.enterprise.dto.Specimen;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -13,7 +14,8 @@ public class PlantDiaryController {
      */
 
     @RequestMapping("/")
-    public String index(){
+    public String index() {
+        Specimen specimen = new Specimen();
         return "start";
     }
 }
